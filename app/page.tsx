8dc99e241033eda9a04b6c5cc936ef4720454a69
abel <abeloabate01@gmail.com>
2024-12-29 21:39:52 +0300

    sd

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import Deposit from "./components/Deposit/page";
 import { useActivePage } from './components/ActivePageContext';
-
+import { AppRoot } from '@telegram-apps/telegram-ui';//
 import Smm from "./components/User/page";
 import Orders from './components/Orders/page';
 import Accounts from './components/Accounts/page';
@@ -13,29 +13,31 @@ const Telegram = () => {
   return (
     <>
 
+      <AppRoot>
 
-      <div className='w-screen' >
+        <div className='w-screen' >
 
-        <div
-          id="1"
-          className={`w-screen ${activePage === 1 ? '' : 'hidden'}`}>
-          <Smm />
-        </div>
-        <div
-          id="2"
-          className={`w-screen ${activePage === 2 ? '' : 'hidden  '} `}><Deposit />
-        </div>
-        <div
-          id="3"
-          className={`w-screen ${activePage === 3 ? '' : 'hidden  '} `}><Orders />
-        </div>
-        <div
-          id="4"
-          className={`w-screen ${activePage === 4 ? '' : 'hidden  '} `}><Accounts />
+          <div
+            id="1"
+            className={`w-screen ${activePage === 1 ? '' : 'hidden'}`}>
+            <Smm />
+          </div>
+          <div
+            id="2"
+            className={`w-screen ${activePage === 2 ? '' : 'hidden  '} `}><Deposit />
+          </div>
+          <div
+            id="3"
+            className={`w-screen ${activePage === 3 ? '' : 'hidden  '} `}><Orders />
+          </div>
+          <div
+            id="4"
+            className={`w-screen ${activePage === 4 ? '' : 'hidden  '} `}><Accounts />
+          </div>
+
         </div>
 
-      </div>
-
+      </AppRoot>
     </>
   );
 };
